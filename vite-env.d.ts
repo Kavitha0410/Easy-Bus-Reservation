717023ecac9862cbccf3a372901fcab5ff3264c4
JAYAVARAM LAKSHMI RANGA KAVITHA SREE <164210439+Kavitha0410@users.noreply.github.com>
2025-05-05 14:16:53 +0530

    Add files via upload

---
+++ vite-env.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="vite/client" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.json' {
+  const content: any;
+  export default content;
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly NODE_ENV: 'development' | 'production' | 'test';
+    readonly VITE_APP_TITLE: string;
+  }
+}
